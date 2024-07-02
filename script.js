029d18d6291d98fe63c4720f5cf31271374ef89e
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-07-02 12:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
-window.onload = function() {
-  const button = document.querySelector("#submit");
-  const pwd = document.querySelector("#password");
-  const name = document.querySelector("#username");
-  const checkBox = document.querySelector("#checkbox");
-
-  class details {
-    constructor(name,password){
-      this.name = name
-      this.password = password
-    }
-  }
-
-  button.addEventListener("click",saveData)
-
-  function saveData(eve) {
-    eve.preventDefault()
-
-    if(checkBox.checked){
-      let provideDetails = new details(name.value,pwd.value)
-
-      let getDetails = localStorage.getItem("details");
-      let rawData = JSON.parse(getDetails);
-
-      if (rawData && name.value === rawData.name) {
-        alert(`You are an existing user`);
-        return;
-      }
-
-      let jsonDetails = JSON.stringify(provideDetails)
-      localStorage.setItem("details",jsonDetails)
-
-      alert(`Logged in as ${name.value}`)
-    } else {
-      localStorage.removeItem("details");
-    }
-  }
-
-  let getDetails = localStorage.getItem("details");
-  let rawData = JSON.parse(getDetails);
-
-  if(rawData) {
-    let newButton = document.createElement("button");
-    newButton.innerText = "Login as existing user"
-    newButton.id = "existing"
-    document.body.append(newButton)
-
-    newButton.addEventListener("click",(eve)=>{
-      eve.preventDefault();
-      alert(`Logged in as ${rawData.name}`)
-    })
-  }
+const usernameInput=document.getElementById('username');
+const passwordInput=document.getElementById('password');
+const checkboxInput=document.getElementById('checkbox');
+const submitButton=document.getElementById('submit');
+const existingButton=document.getElementById('existing');
+ 
+function checkIfUserExistsInLocalStorage() {
+	if(localStorage.getItem('user')){
+		existingButton.style.display='block'
+	}else{
+		existingButton.style.display='none'
+	}
 }
+ 
+checkIfUserExistsInLocalStorage()
+ 
+function saveUserInLocalStorage() {
+	const user={
+		username:usernameInput.value,
+		password:passwordInput.value,
+	}
+ 
+	localStorage.setItem('user',JSON.stringify(user))
+}
+ 
+function removeUserFromLocalStorage() {
+	localStorage.removeItem('user')
+}
+ 
+existingButton.addEventListener('click',(e)=>{
+	e.preventDefault();
+	alert('Logged in as username')
+})
+ 
+submitButton.addEventListener('click',(e)=>{
+	e.preventDefault();
+ 
+	if(!checkboxInput.checked){
+		removeUserFromLocalStorage()
+		// remove existing button
+		checkIfUserExistsInLocalStorage()
+ 
+	}else{
+		saveUserInLocalStorage()
+		// show existing button
+		checkIfUserExistsInLocalStorage()
+	}
+	alert('Logged in as username')
+})
