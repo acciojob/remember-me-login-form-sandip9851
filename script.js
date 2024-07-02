8c3ebe755806b01d60d4bf37d871f8a8b368bb7e
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-07-02 11:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const button = document.querySelector("#submit");
+const pwd = document.querySelector("#password");
+const name = document.querySelector("#username");
+const checkBox = document.querySelector("#checkbox");
+
+class details {
+	constructor(name,pasword){
+		this.name = name
+		this.pasword = pasword
+	}
+}
+
+button.addEventListener("click",saveData)
+
+function saveData(eve) {
+	eve.preventDefault()
+
+	
+	if(checkBox.checked){
+		let provideDetails = new details(name.value,pwd.value)
+
+		let getDetails = localStorage.getItem("details");
+        let rawData = JSON.parse(getDetails);
+
+        if (rawData && name.value === rawData.name) {
+            alert(`You are an existing user`);
+						return;
+        }
+		
+		let jsonDetails = JSON.stringify(provideDetails)
+		localStorage.setItem("details",jsonDetails)
+		
+		alert(`Logged in as ${name.value}`)
+		
+
+	}else{
+		localStorage.removeItem("details");
+	}
+	
+
+	
+	
+}
