15678862f1ba3fef2c423b01ae99e6481f46a7f6
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-07-02 12:11:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-const button = document.querySelector("#submit");
-const pwd = document.querySelector("#password");
-const name = document.querySelector("#username");
-const checkBox = document.querySelector("#checkbox");
+window.onload = function() {
+  const button = document.querySelector("#submit");
+  const pwd = document.querySelector("#password");
+  const name = document.querySelector("#username");
+  const checkBox = document.querySelector("#checkbox");
 
-const newButton = document.querySelector("#existing");
-newButton.style.display = "none"
+  class details {
+    constructor(name,password){
+      this.name = name
+      this.password = password
+    }
+  }
 
-class details {
-	constructor(name,pasword){
-		this.name = name
-		this.pasword = pasword
-	}
+  button.addEventListener("click",saveData)
+
+  function saveData(eve) {
+    eve.preventDefault()
+
+    if(checkBox.checked){
+      let provideDetails = new details(name.value,pwd.value)
+
+      let getDetails = localStorage.getItem("details");
+      let rawData = JSON.parse(getDetails);
+
+      if (rawData && name.value === rawData.name) {
+        alert(`You are an existing user`);
+        return;
+      }
+
+      let jsonDetails = JSON.stringify(provideDetails)
+      localStorage.setItem("details",jsonDetails)
+
+      alert(`Logged in as ${name.value}`)
+    } else {
+      localStorage.removeItem("details");
+    }
+  }
+
+  let getDetails = localStorage.getItem("details");
+  let rawData = JSON.parse(getDetails);
+
+  if(rawData) {
+    let newButton = document.createElement("button");
+    newButton.innerText = "Login as existing user"
+    newButton.id = "existing"
+    document.body.append(newButton)
+
+    newButton.addEventListener("click",(eve)=>{
+      eve.preventDefault();
+      alert(`Logged in as ${rawData.name}`)
+    })
+  }
 }
-
-button.addEventListener("click",saveData)
-
-function saveData(eve) {
-	eve.preventDefault()
-
-	
-	if(checkBox.checked){
-		let provideDetails = new details(name.value,pwd.value)
-
-		let getDetails = localStorage.getItem("details");
-        let rawData = JSON.parse(getDetails);
-
-        if (rawData && name.value === rawData.name) {
-			 alert(`You are an existing user`);
-          newButton.style.display = "block"
-			
-					 
-			newButton.addEventListener("click",(eve)=>{
-				eve.preventDefault();
-		alert(`Logged in as ${rawData.name}`)
-			})
-
-						return;
-        }
-		
-		let jsonDetails = JSON.stringify(provideDetails)
-		localStorage.setItem("details",jsonDetails)
-		
-		alert(`Logged in as ${name.value}`)
-		
-
-	}else{
-		localStorage.removeItem("details");
-	}
-	
-
-	
-	
-}
-
